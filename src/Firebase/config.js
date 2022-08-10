370896e3f0b13c0d3389c007ade1cf83c53a1f7b
Sira Ndiaye <97883169+Sira-Ndiaye@users.noreply.github.com>
2022-08-10 01:31:35 +0000

    Update config.js

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -3,14 +3,14 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7sJvDL4hkanZ8OPQOhInfWcqIsnaQ8SE",
-  authDomain: "sb-l-218a4.firebaseapp.com",
-  databaseURL: "https://sb-l-218a4.firebaseio.com",
-  projectId: "sb-l-218a4",
-  storageBucket: "sb-l-218a4.appspot.com",
-  messagingSenderId: "225327984690",
-  appId: "1:225327984690:web:82e9484ad4129dc8990c2b",
-  measurementId: "G-RKH3YN3HBW"
+  apiKey: "apiKey",
+  authDomain: "***.firebaseapp.com",
+  databaseURL: "https://***.firebaseio.com",
+  projectId: "***",
+  storageBucket: "***.appspot.com",
+  messagingSenderId: "***",
+  appId: "***",
+  measurementId: "***"
 };
 
 if (!firebase.apps.length) {
